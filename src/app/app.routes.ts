8ebb7ee08a35e5410810bd0da82ff14d768d7ef5
Edgard Barquero <ebarquero85@gmail.com>
2024-12-12 16:20:03 -0600

    Se ejecuta comando para formato

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: "",
-        redirectTo: "/home",
-        pathMatch: "full"
-    },
-    {
-        path: "home",
-        loadComponent: () => import("./modules/example-page/example-page.component").then((m) => m.ExamplePageComponent)
-
-    }
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full',
+	},
+	{
+		path: 'home',
+		loadComponent: () => import('./modules/example-page/example-page.component').then((m) => m.ExamplePageComponent),
+	},
 ];
