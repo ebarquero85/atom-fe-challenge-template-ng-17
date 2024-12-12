d8301e895969ebe4467d73e0a67b5e6a4f71fc1e
Edgard Barquero <ebarquero85@gmail.com>
2024-12-12 17:31:50 -0600

    Se agrega módulo de auth y el componente login con sus validaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,12 @@ export const routes: Routes = [
 		path: 'home',
 		loadComponent: () => import('./modules/example-page/example-page.component').then((m) => m.ExamplePageComponent),
 	},
+	{
+		path: 'auth',
+		loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+	},
+	{
+		path: '**',
+		redirectTo: 'auth/login',
+	},
 ];
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+	selector: 'app-login',
+	standalone: true,
+	imports: [CommonModule, ReactiveFormsModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatGridListModule],
+	templateUrl: './login.component.html',
+	styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+	loginForm: FormGroup;
+
+	constructor(private fb: FormBuilder) {
+		this.loginForm = this.fb.group({
+			email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+		});
+	}
+
+	getEmailErrorMessage() {
+		const email = this.loginForm.get('email');
+		if (email?.hasError('required')) {
+			return 'El correo es obligatorio.';
+		}
+		if (email?.hasError('pattern')) {
+			return 'El correo no tiene un formato válido.';
+		}
+		return '';
+	}
+
+	onSubmit() {
+		if (this.loginForm.valid) {
+			console.log('Formulario enviado:', this.loginForm.value);
+		}
+	}
+}
