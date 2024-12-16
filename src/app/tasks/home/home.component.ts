05cbba126a1130c11a0cb56f3481b800d61410e7
Edgard Barquero <ebarquero85@gmail.com>
2024-12-16 13:33:31 -0600

    Se mejora el responsive y el estilo

--- src/app/tasks/home/home.component.ts
+++ src/app/tasks/home/home.component.ts
@@ -7,11 +7,21 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { Router } from '@angular/router';
 import { TodoListComponent } from '../todo-list/todo-list.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [TodoFormComponent, TodoListComponent, MatCardModule, MatGridListModule, MatIconModule, MatDividerModule, MatFormFieldModule],
+	imports: [
+		TodoFormComponent,
+		TodoListComponent,
+		MatCardModule,
+		MatGridListModule,
+		MatIconModule,
+		MatDividerModule,
+		MatFormFieldModule,
+		MatButtonModule,
+	],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.scss',
 })
